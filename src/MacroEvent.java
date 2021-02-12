2626a70d8049e9a4502f4fe197bb8b9bdee97446
The_Mach34 <masterjude5000@gmail.com>
2021-02-11 18:40:04 -0800

    Added methods and more variables

--- src/MacroEvent.java
+++ src/MacroEvent.java
@@ -1,34 +1,63 @@
 public class MacroEvent {
 
     private String mActionType;
-    private int mDelay;
+    private double mDelay;
+    private char mKey;
+    private int mXCoord;
+    private int mYCoord;
+    private int mMouseButtonN;
 
     // Delay
 
     public MacroEvent(String actionType, double delay) {
 
         mActionType = actionType;
+        mDelay = delay;
 
     }
 
-    // Key press/release
+    // Key action
 
     public MacroEvent(String actionType, char key) {
 
         mActionType = actionType;
+        mKey = key;
 
     }
 
-    // Mouse click
+    // Mouse action
 
-    public MacroEvent(String actionType, int xCoord, int yCoord, int clickType) {
+    public MacroEvent(String actionType, int xCoord, int yCoord, int mouseButtonN) {
 
         mActionType = actionType;
+        mXCoord = xCoord;
+        mYCoord = yCoord;
+        mMouseButtonN = mouseButtonN;
 
     }
 
     public String getActionType() {
         return mActionType;
+    }
+
+    public Double getDelayTime() {
+        return mDelay;
+    }
+
+    public char getKey() {
+        return mKey;
+    }
+
+    public int getXCoord() {
+        return mXCoord;
+    }
+
+    public int getYCoord() {
+        return mYCoord;
+    }
+
+    public int getMouseButtonN() {
+        return mMouseButtonN;
     }
 
 }
